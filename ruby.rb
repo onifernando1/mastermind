92171671fdfcb20f01621ee7aa819b30e1392344
onifernando1 <onifernando1@gmail.com>
2022-07-06 22:01:29 +0100

    Update game logic to check colour and position

--- ruby.rb
+++ ruby.rb
@@ -5,6 +5,7 @@
 # 3. If colour in right place = O if right colour wrong place X
 # 4. If person guesses color = win
 # 5. 12 turns
+# Note logic is wrong :
 
 class Game
   attr_accessor :game_running
@@ -12,8 +13,6 @@ class Game
   def initialize(comp_selection, player_selection)
     @comp_selection = comp_selection
     @player_selection = player_selection
-    # p "#{@comp_selection} from Game class "
-    # p @player_selection
     @game_running = true
     @board = []
   end
@@ -26,30 +25,22 @@ class Game
   end
 
   def check_position_and_colour
- 
-    @player_selection.each_index {|index| 
-      if @comp_selection[index] == @player_selection[index] 
-        @board << "O"
-      elsif @player_selection.include? @comp_selection[index]
-        @board << "X" 
-      end 
-    }
-
-  end 
- 
-
-
+    @player_selection.each_index do |index|
+      if @comp_selection[index] == @player_selection[index]
+        @board << 'O'
+      elsif @player_selection[index] == @comp_selection[0] || @player_selection[index] == @comp_selection[1] ||@player_selection[index] == @comp_selection[2] ||@player_selection[index] == @comp_selection[3]
+        @board << "X"
+      end
+    end
+  end
 
   def display_board
     p @board
-    puts "\n" 
-  end 
-
+    puts "\n"
+  end
 end
 
 class Computer
-  # attr_accessor :comp_selection
-
   def initialize
     @comp_selection = []
   end
@@ -60,64 +51,55 @@ class Computer
       @rand_num = rand 0..5
       @colour_options = %w[Red Blue Yellow Orange Purple Green]
       @comp_selection << @colour_options[@rand_num]
-      # p @comp_selection
       i += 1
     end
-
+    p @comp_selection
     @comp_selection
   end
 end
 
-class Player 
-
+class Player
   attr_accessor :round
 
-  def initialize 
+  def initialize
     @round = 0
-  end 
-
+  end
 
   def player_input
     puts "Guess a combination of 4 colours (Red, Blue, Yellow, Orange, Purple, Green)
 with each colour separated by a space.
-For example: Red Blue Green Orange"
+For example: Red Blue Green Orange
+If colour in right place, you get O. If it is the right colour, but wrong place you get X"
     @player_selection = gets.chomp
-    @round += 1 
+    @round += 1
     puts "round #{@round}"
   end
 
   def player_choice
     @player_selection = @player_selection.split(' ')
-    p "Player Selection:#{@player_selection}"
+    puts "Player Selection:#{@player_selection}"
     @player_selection = @player_selection.map(&:capitalize)
     @player_selection
   end
-
-  def game_check
-    if game.game_running == false 
-      puts "ASDLJAKSDLKASJD"
-    end 
-  end 
 end
 
 class RunLogic
+  
+  def initialize 
+    computer = Computer.new
+    comp_selection = computer.random_selection
+    player = Player.new
 
-  computer = Computer.new
-  comp_selection = computer.random_selection
-  player = Player.new
-
-  until player.round == 12 || 
-    player.player_input
-    player_choice = player.player_choice()
-    game = Game.new(comp_selection, player_choice)
-    game.check_win
-    game.check_position_and_colour
-    game.display_board
-    if game.game_running == false 
-      break
-    end 
+    until player.round == 12
+      player.player_input
+      player_choice = player.player_choice()
+      game = Game.new(comp_selection, player_choice)
+      game.check_win
+      game.check_position_and_colour
+      game.display_board
+      break if game.game_running == false
+    end
   end 
+end
 
-end 
-
-RunLogic.new()
+RunLogic.new
